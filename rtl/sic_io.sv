9b4a0e2e30d6e9d1d66b48b5bbaae18a77bb8f68
Akash <akashmishra.am.1710@gmail.com>
2025-05-17 15:24:33 +0530

    Added i/o facility and wrapped everything inside a top sic module

---
+++ rtl/sic_io.sv
@@ -0,0 +1,34 @@
+`ifndef SIC_IO_SV
+`define SIC_IO_SV
+
+module moduleName (
+    input logic clk,
+    input logic rst,
+    input logic read_enable,
+    input logic write_enable,
+    output logic device_ready,
+    input logic [7:0] in_data,
+    output logic [7:0] out_data,
+    output logic write_event
+);
+    
+    always_ff @(posedge clk or posedge rst) begin 
+
+        if(rst) begin 
+            out_data <= 8'd0;
+            write_event <= 1'b0;
+        end else begin 
+            if (write_enable) begin 
+                out_data <= in_data;
+                write_event <= 1'b1;
+            end else begin 
+                write_event <= 1'b0;
+            end
+        end
+    end
+
+    //device always ready
+    assign device_ready = 1'b1;
+endmodule
+
+`endif
---
+++ rtl/sic_top.sv
@@ -0,0 +1,43 @@
+`ifndef SIC_TOP_SV
+`define SIC_TOP_SV
+
+module moduleName #(
+    parameter ADDRESS_WIDTH = 15,
+    parameter DATA_WIDTH = 24
+)(
+    input logic clk,
+    input logic rst
+);
+    
+    logic [DATA_WIDTH-1:0] instruction;
+    logic [ADDRESS_WIDTH-1:0] memory_address;
+    logic [DATA_WIDTH-1:0] memory_read_data, memory_write_data;
+    logic memory_write_enable;
+
+    // Memory and CPU
+    sic_memory u_mem(
+    .clk(clk),
+    .write_enable(memory_write_enable),
+    .address(memory_address),
+    .memory_write_data(memory_write_data),
+    .memory_read_data(memory_read_data)
+  );
+
+  sic_cpu #(
+    .ADDR_WIDTH(ADDRESS_WIDTH),
+    .DATA_WIDTH(DATA_WIDTH)
+  ) u_cpu(
+    .clk(clk),
+    .rst(rst),
+    .instruction(instruction),
+    .memory_address(memory_address),
+    .memory_read_data(memory_read_data),
+    .memory_write_data(memory_write_data),
+    .write_enable(memory_write_enable)
+  );
+
+    //fetch current instruction
+    assign instruction = memory_read_data;
+endmodule
+
+`endif
